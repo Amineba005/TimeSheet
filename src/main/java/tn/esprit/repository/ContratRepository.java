b48548686f43453bb1b2494965a16af020686aa5
naada12 <49064984+naada12@users.noreply.github.com>
2021-10-26 14:08:00 +0100

    repository

---
+++ src/main/java/tn/esprit/repository/ContratRepository.java
@@ -0,0 +1,9 @@
+package tn.esprit.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tn.esprit.spring.entities.Contrat;
+
+public interface ContratRepository extends CrudRepository<Contrat, Integer>{
+
+} 
