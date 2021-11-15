94696c613cb36fc2cd256ee7ff075670f91039e3
naada12 <49064984+naada12@users.noreply.github.com>
2021-11-15 14:40:13 +0000

    squelette

--- src/main/java/tn/esprit/repository/ContratRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import tn.esprit.entities.Contrat;
-
-public interface ContratRepository extends CrudRepository<Contrat, Integer>{
-
-} 
--- src/main/java/tn/esprit/repository/DepartementRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import tn.esprit.entities.Departement;
-
-public interface DepartementRepository extends CrudRepository<Departement, Integer>{
-
-}
